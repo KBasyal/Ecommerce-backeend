46e1d4a9b0eba458e810643f051b458d900cbe54
kbasyal <kamalbasyal987@gmial.com>
2024-04-03 21:09:19 +0545

    express and routing intro

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -3,14 +3,31 @@ const app = express()
 
 
 // routing
-app.use("/",(request , response)=>{
-    // response.send("hello world")
+app.get("/about", (resquest, response)=>{
+    //only for the get request
     response.json({
-        result: "data",
-        message :"success",
-        meta : null
+        result:"any data",
+        message:"this is about page",
+        meta:null
     })
-});
+})
+
+// 404 route
+// app.use("/:productSlug",(request , response)=>{
+//     // response.send("hello world")
+//     response.status(404).json({
+//         result: "data",
+//         message :"success",
+//         meta : null
+//     })
+// });
+app.use("/" , (request, response)=>{
+    response.status(404).json({
+        result :"any",
+        message:"hello there",
+        meta:null
+    })
+})
 
 
 
